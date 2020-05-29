3ab4383680ddc7daa099e9fb5c73bdebde21d640
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2020-05-29 23:06:39 +0000

    Added boostrap theme

--- EnsafMarket.WebApp/App_Start/BundleConfig.cs
+++ EnsafMarket.WebApp/App_Start/BundleConfig.cs
@@ -8,32 +8,6 @@ namespace EnsafMarket.WebApp
         // Pour plus d'informations sur le regroupement, visitez https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilisez la version de développement de Modernizr pour le développement et l'apprentissage. Puis, une fois
-            // prêt pour la production, utilisez l'outil de génération à l'adresse https://modernizr.com pour sélectionner uniquement les tests dont vous avez besoin.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-            #region Template Desing
-
-
-            bundles.Add(new StyleBundle("~/template/css").Include(
-                      "~/Content/css/bootstrap.css",
-                      "~/Content/css/style.css",
-                      "~/Content/css/font-awesome.min.css"
-                      ));
-            #endregion
         }
     }
 }
