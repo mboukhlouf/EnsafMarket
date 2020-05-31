89f0829af59a8873deaa4edc7dba4b5d60c13dad
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2020-05-31 15:21:59 +0100

    Added admin dashboard template

---
+++ EnsafMarket.WebApp/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+using EnsafMarket.WebApp.Areas.Admin.Controllers.Abstract;
+
+namespace EnsafMarket.WebApp.Areas.Admin.Controllers
+{
+    public class LoginController : BaseController
+    {
+        public async Task<ActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
